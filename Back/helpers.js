74f65cef65aa256c60ac23b2e3f7636720b54fd3
Daniel Alejandro Farneda <alejandrofarneda@gmail.com>
2021-06-02 16:02:02 +0200

    clonado funcionando

--- Back/helpers.js
+++ Back/helpers.js
@@ -7,56 +7,56 @@ const { nanoid } = require('nanoid');
 sendgrid.setApiKey(process.env.SENDGRID_API);
 
 async function sendMail({ to, subject, message }) {
-  try {
-    const msg = {
-      to,
-      subject,
-      from: process.env.SENDGRID_FROM,
-      text: message,
-    };
+    try {
+        const msg = {
+            to,
+            subject,
+            from: process.env.SENDGRID_FROM,
+            text: message,
+        };
 
-    await sendgrid.send(msg);
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error enviando mail');
-  }
+        await sendgrid.send(msg);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error enviando mail');
+    }
 }
 
 //Creamos la ruta al directorio de uploads
 const uploadsDir = path.join(__dirname, process.env.UPLOADS_DIRECTORY);
 
 async function deleteAvatar({ file }) {
-  const avatarPath = path.join(uploadsDir, file);
-  await fs.unlink(avatarPath);
+    const avatarPath = path.join(uploadsDir, file);
+    await fs.unlink(avatarPath);
 }
 
 async function saveAvatar({ file }) {
-  try {
-    //Nos aseguramos de que el directorio existe
-    await fs.mkdir(uploadsDir, { recursive: true });
+    try {
+        //Nos aseguramos de que el directorio existe
+        await fs.mkdir(uploadsDir, { recursive: true });
 
-    //Cargamos la imagen en sharp
-    const image = sharp(file.data);
+        //Cargamos la imagen en sharp
+        const image = sharp(file.data);
 
-    //Modificamos el tamaño de la imagen a un estandar
-    image.resize(500);
+        //Modificamos el tamaño de la imagen a un estandar
+        image.resize(500);
 
-    //Creamos un nombre de fichero aleatorio para la imagen
-    const fileName = `${nanoid(20)}.jpg`;
+        //Creamos un nombre de fichero aleatorio para la imagen
+        const fileName = `${nanoid(20)}.jpg`;
 
-    //Guardamos la imagen en el directorio uploads
-    await image.toFile(path.join(uploadsDir, fileName));
+        //Guardamos la imagen en el directorio uploads
+        await image.toFile(path.join(uploadsDir, fileName));
 
-    //Devolvemos el nombre de fichero
-    return fileName;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error subiendo imagen');
-  }
+        //Devolvemos el nombre de fichero
+        return fileName;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error subiendo imagen');
+    }
 }
 
 module.exports = {
-  sendMail,
-  saveAvatar,
-  deleteAvatar,
+    sendMail,
+    saveAvatar,
+    deleteAvatar,
 };
